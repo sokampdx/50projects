6b22bf30b994731246391d93cb3db5954c44cbe8
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-05-15 00:10:09 -0700

    project 24 fix bug

--- project24_content_placeholder/script.js
+++ project24_content_placeholder/script.js
@@ -2,7 +2,7 @@ const header = document.getElementById('header')
 const title = document.getElementById('title')
 const excerpt = document.getElementById('excerpt')
 const profile_img = document.getElementById('profile_img')
-const name = document.getElementById('name')
+const author = document.getElementById('author')
 const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
@@ -10,12 +10,12 @@ const animated_bg_texts = document.querySelectorAll('animated-bg-text')
 
 setTimeout(getData, 2500)
 
-function getDate() {
+function getData() {
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=800" alt="">'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
     excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, delectus!'
     profile_img.innerHTML = '<img src="https://randomuser.me/api/portraits/women/45.jpg" alt="">'
-    name.innerHTML = 'Jane Doe'
+    author.innerHTML = 'Jane Doe'
     date.innerHTML = 'Nov 4, 1999'
 
     animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
