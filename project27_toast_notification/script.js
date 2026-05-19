a5737729258db99a7c17a8979e60635dce491f18
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-05-19 13:38:24 -0700

    completed project 27

---
+++ project27_toast_notification/script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('button')
+const container = document.getElementById('toasts')
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+]
+
+const type = ['info', 'success', 'error']
+
+
+btn.addEventListener('click', () => createNotification())
+
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
+    notif.innerText = message ? message : getRandomMessage()
+    container.appendChild(notif)
+
+    setTimeout(() => {notif.remove()}, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+    return type[Math.floor(Math.random() * type.length)]
+}
